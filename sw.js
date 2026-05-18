113c6e62db90d61cb5491e678b33bb19a21f02c4
michlbaur-creator <michlbaur@googlemail.com>
2026-05-18 19:47:05 +0200

    Sprechblase

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v4';
+const CACHE_NAME = 'mibaso-natur-v5';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
