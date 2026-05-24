97dec04720881b145afb8bc84ab114dbdd46f36b
michlbaur-creator <michlbaur@googlemail.com>
2026-05-24 17:47:21 +0200

    photosynthese interakive

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v23';
+const CACHE_NAME = 'mibaso-natur-v24';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
