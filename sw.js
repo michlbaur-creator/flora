f7697e93118015d114d357829b8dc5c144dd42df
michlbaur-creator <michlbaur@googlemail.com>
2026-05-24 18:32:55 +0200

    Atmung neu

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v24';
+const CACHE_NAME = 'mibaso-natur-v25';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
