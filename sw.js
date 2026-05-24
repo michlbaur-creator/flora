18b734d769766fdd12b34661b2e328bb51b14fe1
michlbaur-creator <michlbaur@googlemail.com>
2026-05-24 17:31:26 +0200

    seitenaufbau geändert neue Photosynthese

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v22';
+const CACHE_NAME = 'mibaso-natur-v23';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
