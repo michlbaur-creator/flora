4f9c0e415398600687c44d13b1d94b7f9ee99994
michlbaur-creator <michlbaur@googlemail.com>
2026-05-25 13:56:36 +0200

    Wortbank

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v25';
+const CACHE_NAME = 'mibaso-natur-v26';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
