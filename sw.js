7a94a3ad8dd422c526b8daacb12d37763cbd61a3
michlbaur-creator <michlbaur@googlemail.com>
2026-05-26 09:34:39 +0200

    austausch tafeln und bilder

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v31';
+const CACHE_NAME = 'mibaso-natur-v32';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
