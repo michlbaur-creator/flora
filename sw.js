7716b179eef12701335ac4b07df551edc5f3fc5e
michlbaur-creator <michlbaur@googlemail.com>
2026-05-28 12:31:53 +0200

    größenänderung

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v36';
+const CACHE_NAME = 'mibaso-natur-v37';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
