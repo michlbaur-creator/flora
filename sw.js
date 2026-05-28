18dce48d344a09071ebfcb81e741d1ec2c159fad
michlbaur-creator <michlbaur@googlemail.com>
2026-05-28 12:53:08 +0200

    farbe

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v37';
+const CACHE_NAME = 'mibaso-natur-v38';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
