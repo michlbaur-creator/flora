0189cc00534a5b4d1a2ea660cea744363dc35a12
michlbaur-creator <michlbaur@googlemail.com>
2026-05-29 10:43:06 +0200

    Systematik Quiz

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v38';
+const CACHE_NAME = 'mibaso-natur-v40';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
