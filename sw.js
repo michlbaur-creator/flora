12a44530bc824ebddaa69de4c35504961c7ad826
michlbaur-creator <michlbaur@googlemail.com>
2026-06-11 10:22:41 +0200

    Header Korrektur2

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v51';
+const CACHE_NAME = 'mibaso-natur-v52';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
