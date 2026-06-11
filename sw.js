8131a600e81f8c294fa81aad606c8e4cdf70b9c8
michlbaur-creator <michlbaur@googlemail.com>
2026-06-11 11:12:55 +0200

    Gesamt-Quiz

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v53';
+const CACHE_NAME = 'mibaso-natur-v55';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
