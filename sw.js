1e5df13edf6fbe8bb58b9278e433ffe79cc4ebed
michlbaur-creator <michlbaur@googlemail.com>
2026-06-13 08:30:30 +0200

    korrektur

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v69';
+const CACHE_NAME = 'mibaso-natur-v70';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest).
 const APP_SHELL = [
