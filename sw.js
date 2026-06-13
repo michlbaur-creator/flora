8dc10e0dfe9e2f90e0c01f82bd1e3b609e89a3b8
michlbaur-creator <michlbaur@googlemail.com>
2026-06-13 10:04:10 +0200

    backlink

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v76';
+const CACHE_NAME = 'mibaso-natur-v78';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest + Pflanzendaten).
 const APP_SHELL = [
