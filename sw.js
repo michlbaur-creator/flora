531b92892fd00da61aa156787b524725cd84fbd7
michlbaur-creator <michlbaur@googlemail.com>
2026-06-13 10:49:42 +0200

    Button Änderung

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v79';
+const CACHE_NAME = 'mibaso-natur-v80';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest + Pflanzendaten).
 const APP_SHELL = [
