46f2f04d0cd508c295f3393ddf739c96c584d002
michlbaur-creator <michlbaur@googlemail.com>
2026-06-14 06:41:40 +0200

    neue Arten

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    die alten Dateien automatisch auf.
 */
 
-const CACHE_NAME = 'mibaso-natur-v88';
+const CACHE_NAME = 'mibaso-natur-v89';
 
 // Wichtige Dateien, die für den App-Start gebraucht werden (Hub + Icons + Manifest + Pflanzendaten).
 const APP_SHELL = [
