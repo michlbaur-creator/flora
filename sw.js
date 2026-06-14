452a5cd5a919080beb3e9a461a1534623f85ed7f
michlbaur-creator <michlbaur@googlemail.com>
2026-06-14 11:25:00 +0200

    broschüren comp

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'mibaso-shell-v92';
+const SHELL_CACHE = 'mibaso-shell-v93';
 const MEDIA_CACHE = 'mibaso-media-v1';
 const MANIFEST_URL = './offline-manifest.json';
 
